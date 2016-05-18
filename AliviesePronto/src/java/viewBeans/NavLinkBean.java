4f18df1ae9fffb7b83b569ed07f35e1ee3d821fd
Andres Felipe Leon Cruz <afleonc@gmail.com>
2016-05-17 22:40:51 -0500

    Actualizado BootsFaces incluido login de farmaceuta administrador y gerente

--- AliviesePronto/src/java/viewBeans/NavLinkBean.java
+++ AliviesePronto/src/java/viewBeans/NavLinkBean.java
@@ -1,48 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package viewBeans;
 
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+/**
+ *
+ * @author afleonc
+ */
 @ViewScoped
 @ManagedBean
 public class NavLinkBean implements Serializable {
-	private static final long serialVersionUID = 1L;
 
-	private int activePage = 0;
-
-	private String source;
-
-	public void navigateToPage(int page, String source) {
-		activePage = page;
-                System.out.println(activePage);
-		this.source = source;
-                System.out.println(this.source);
-	}
-
-	public String getPageTitle() {
-		return "Page " + (activePage + 1);
-	}
-
-	public String getPageContent() {
-            System.out.println(source);
-		if (source == null) {
-			return "loginMedico.xhtml";
-		}
-		if (0 == activePage)
-			return "loginMedico.xhtml";
-		if (1 == activePage)
-			return "inicio.xhtml";
-		return "This is an unknown page.";
-	}
-        
-        public String sendPageContent(){
-            return "loginMedico.xhtml";
+    public String navegacion(String tipoUsuario) {
+        System.out.println(tipoUsuario);
+        if (tipoUsuario.equals("Paciente")) {
+            return "crearPaciente.xhtml";
         }
+        if (tipoUsuario.equals("Medico")) {
+            return "crearMedico.xhtml";
+        } 
+        else{
+            return "crearFarmaceuta.xhtml";
+        }
+    }
 
-	public String getPageLook() {
-		if (0 == activePage)
-			return "primary";
-		return "success";
-	}
 }
